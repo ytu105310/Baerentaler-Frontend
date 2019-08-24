5d1ebcf5fa730cd3f0b20c1f8c5a3550437ae445
Vanessa Eberhard <vanessa.eberhard@swisscom.com>
2019-08-24 21:45:23 +0200

    Chart

--- baerentaler/screens/ProfileScreen.js
+++ baerentaler/screens/ProfileScreen.js
@@ -1,16 +1,60 @@
 import React from 'react';
+import { Dimensions } from 'react-native'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
 
 import {
   ScrollView,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.info}>
-      </ScrollView>
+      <Text>
+    Bezier Line Chart
+  </Text>
+  <LineChart
+    data={{
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [{
+        data: [
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100
+        ]
+      }]
+    }}
+    width={Dimensions.get('window').width} // from react-native
+    height={220}
+    yAxisLabel={'$'}
+    chartConfig={{
+      backgroundColor: '#e26a00',
+      backgroundGradientFrom: '#fb8c00',
+      backgroundGradientTo: '#ffa726',
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
     </View>
   );
 }
