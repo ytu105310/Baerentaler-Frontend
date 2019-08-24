23646100e5d2c56b161a309f2b710e92144c00b4
Leo Schöchli <leo.schoechli@swisscom.com>
2019-08-24 21:48:28 +0200

    Added balance counter

--- baerentaler/screens/HomeScreen.js
+++ baerentaler/screens/HomeScreen.js
@@ -3,32 +3,66 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
-  Image
+  Image,
+  TouchableOpacity
 } from 'react-native';
+import { Overlay } from "react-native-elements";
 import Colors from '../constants/Colors';
 
 export default class HomeScreen extends React.Component {
 
-  pay = () => {
+  priceToPay = 4;
 
+  state = {
+    balance: 32,
+    overlayIsVisible: false
+  }
+
+  pay = () => {
+    this.setState({ overlayIsVisible: true });
+  }
+
+  confirm = () => {
+    setTimeout(() => {
+      this.setState({ overlayIsVisible: false });
+    }, 800);
+    this.setState({ balance: this.state.balance - this.priceToPay });
+  }
+
+  refuse = () => {
+    this.setState({ overlayIsVisible: false });
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <Overlay isVisible={this.state.overlayIsVisible}>
+          <View style={styles.overlayContainer}>
+            <Text style={styles.overlayPrice}>{this.priceToPay} Bärentaler</Text>
+            <Text style={styles.overlayText}>bezahlen?</Text>
+            <View style={styles.yesNo}>
+              <TouchableOpacity onPress={this.confirm}>
+                <Image style={styles.yesNoImage} source={require('../assets/images/ok.png')} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.refuse}>
+                <Image style={styles.yesNoImage} source={require('../assets/images/cancel.png')} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Overlay>
         <View style={styles.balanceWrapper}>
           <Text style={styles.balanceText}>Sie haben</Text>
-          <Text style={styles.balance}>32 Bärentaler</Text>
+          <Text style={styles.balance}>{this.state.balance} Bärentaler</Text>
         </View>
         <View style={styles.payWrapper}>
-          <Button
+          <TouchableOpacity
             onPress={this.pay}
-            title="Bezahlen"
-            color={Colors.tintColor} />
+            style={styles.payButton}>
+            <Text style={styles.payText}>Bezahlen</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.chartWrapper}>
-        <Image style={styles.chart} source={require('../assets/images/chart.png')} />
+          <Image style={styles.chart} source={require('../assets/images/chart.png')} />
         </View>
       </View>
     );
@@ -45,6 +79,27 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
+  overlayContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlayPrice: {
+    fontSize: 30,
+  },
+  overlayText: {
+    fontSize: 20,
+  },
+  yesNo: {
+    marginTop: '10%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  yesNoImage: {
+    width: 60,
+    height: 60,
+    margin: 10,
+  },
   balanceWrapper: {
     flex: 1,
     justifyContent: 'center',
@@ -60,6 +115,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  payButton: {
+    paddingHorizontal: '10%',
+    paddingVertical: '4%',
+    backgroundColor: Colors.tintColor,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#000',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  payText: {
+    fontSize: 20,
   },
   chartWrapper: {
     flex: 2,
