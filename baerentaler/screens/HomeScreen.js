5adfea343c236de2c4d1a4c5aed0cd7aa88876b2
Vanessa Eberhard <vanessa.eberhard@swisscom.com>
2019-08-24 21:51:49 +0200

    chart :-)

--- baerentaler/screens/HomeScreen.js
+++ baerentaler/screens/HomeScreen.js
@@ -6,6 +6,15 @@ import {
   Button,
   Image
 } from 'react-native';
+import { Dimensions } from 'react-native'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
 import Colors from '../constants/Colors';
 
 export default class HomeScreen extends React.Component {
@@ -30,6 +39,39 @@ export default class HomeScreen extends React.Component {
         <View style={styles.chartWrapper}>
         <Image style={styles.chart} source={require('../assets/images/chart.png')} />
         </View>
+        <LineChart
+    data={{
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [{
+        data: [
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100
+        ]
+      }]
+    }}
+    width={Dimensions.get('window').width} // from react-native
+    height={220}
+    yAxisLabel={'$'}
+    chartConfig={{
+      backgroundColor: '#e26a00',
+      backgroundGradientFrom: '#fb8c00',
+      backgroundGradientTo: '#ffa726',
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
       </View>
     );
   }
