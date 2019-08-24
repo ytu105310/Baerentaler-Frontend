345d217cf4f456ab00bdbd8dfeeedf3c2fac0ef4
Leo Schöchli <leo.schoechli@swisscom.com>
2019-08-24 14:07:46 +0200

    Removed home screen

--- baerentaler/navigation/MainTabNavigator.js
+++ baerentaler/navigation/MainTabNavigator.js
@@ -3,46 +3,16 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
 import AboScreen from '../screens/AboScreen';
 import ExploreScreen from '../screens/ExploreScreen';
 import ProductScreen from '../screens/ProductScreen';
 import FinanceScreen from '../screens/FinanceScreen';
 import ProfilScreen from '../screens/ProfilScreen';
-import EventScreen from '../screens/EventScreen';
-import StandMonthScreen from '../screens/StandMonthScreen';
-import NewAboScreen from '../screens/NewAboScreen';
-import InfoStandScreen from '../screens/InfoStand';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
   default: {},
 });
-
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Event: EventScreen,
-    StandMonth: StandMonthScreen
-  },
-  config
-);
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-HomeStack.path = '';
 
 const ExploreStack = createStackNavigator(
   {
@@ -137,14 +107,7 @@ ProfilStack.navigationOptions = {
 
 ProfilStack.path = '';
 
-const AppNavigator = createStackNavigator(
-  {
-    Event: EventScreen,
-  },
-);
-
 const tabNavigator = createBottomTabNavigator({
-  HomeStack,
   ExploreStack,
   AboStack,
   FinanceStack,
